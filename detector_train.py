13acd386075fc0491767716bb81f4de198757a5c
Michael Hannalla <michael.sami.hanna@gmail.com>
2021-08-12 19:19:23 +0200

    Added initial training scripts

---
+++ detector_train.py
@@ -0,0 +1,21 @@
+# File: detector_train.py
+# Author: @MichaelHannalla
+# Project: Medicine Packages Sticker Detection
+# Description: Python file for training the detector based on detecto on a single input image
+
+from detecto.core import Dataset, Model
+
+def main():
+
+    #TODO: using the argparser
+    train_dataset = Dataset('data/train')
+    model = Model(['medicament', 'vignette'])
+    val_dataset = Dataset('data/test')
+    losses = model.fit(train_dataset, val_dataset, epochs=100, learning_rate=0.01,
+                   gamma=0.2, lr_step_size=5, verbose=True)
+
+    
+    model.save('models/detector_weights.pth')
+
+if __name__ == "__main__":
+    main()
